35725a28bfb30237f15e31c0ce5975720f433fa2
yanwhocodes <ynawhocodes@gmail.com>
2023-08-19 22:53:17 +0900

    [feat] compcard page UI

---
+++ apps/web/src/app/(pages)/compcard/page.tsx
@@ -0,0 +1,37 @@
+const CompcardPage = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center py-[120px]">
+        <div>
+          <img src="/images/compcard_title.png" alt="compcard" />
+          <div className="flex justify-center">
+            <a href="">
+              <img className="p-1" src="/images/insta_icon.png" alt="insta" />
+            </a>
+            <a href="">
+              <img className="p-1" src="/images/mail_icon.png" alt="mail" />
+            </a>
+            <a href="">
+              <img
+                className="p-1"
+                src="/images/youtube_icon.png"
+                alt="youtube"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="px-4 py-2.5 grid grid-cols-3 gap-2.5 lg:w-3/4 w-full">
+          <div className="aspect-[1/1] bg-gray-300"></div>
+          <div className="aspect-[1/1] bg-gray-300"></div>
+          <div className="aspect-[1/1] bg-gray-300"></div>
+          <div className="aspect-[1/1] bg-gray-300"></div>
+          <div className="aspect-[1/1] bg-gray-300"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CompcardPage;
