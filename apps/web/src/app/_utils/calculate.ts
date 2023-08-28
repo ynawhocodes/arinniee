08864f2ece9bb6052e145aa72d4288b203ae74f6
yanwhocodes <ynawhocodes@gmail.com>
2023-08-28 12:10:09 +0900

    [feat] calculate util 추가

---
+++ apps/web/src/app/_utils/calculate.ts
@@ -0,0 +1,42 @@
+import { BrowserCoordinates } from "../_type/BrowserCoordinates";
+
+export const calculateBrowserCoordinates = (
+  browserWidth: number,
+  browserHeight: number
+): BrowserCoordinates => {
+  const minX = -browserWidth / 2;
+  const minY = -browserHeight / 2;
+  const maxX = browserWidth / 2;
+  const maxY = browserHeight / 2;
+
+  return {
+    minX,
+    minY,
+    maxX,
+    maxY,
+  };
+};
+
+interface RandomPoint {
+  x: number;
+  y: number;
+}
+
+export const getRandomInt = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const getRandomPointInRange = (
+  minX: number,
+  minY: number,
+  maxX: number,
+  maxY: number
+): RandomPoint => {
+  const randomX = getRandomInt(minX, maxX);
+  const randomY = getRandomInt(minY, maxY);
+
+  return {
+    x: randomX,
+    y: randomY,
+  };
+};
