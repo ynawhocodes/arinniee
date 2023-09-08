b4d4bff2c9a480dfb80453f409cada8169fd2fa2
ynawhocodes <ynawhocodes@gmail.com>
2023-09-08 19:09:37 +0900

    [feat] shop type 정의

---
+++ apps/web/src/app/_type/shop.ts
@@ -0,0 +1,10 @@
+export interface ShopType {
+  id: string;
+  createdAt: string;
+  title: string;
+  description: string;
+  price: string;
+  thumbnailImageUrl: string;
+  imageUrl: string;
+  isSoldout: boolean;
+}
