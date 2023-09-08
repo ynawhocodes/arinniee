6980965e1fbd76457543db2cd8c462cc14d12915
ynawhocodes <ynawhocodes@gmail.com>
2023-09-08 19:11:01 +0900

    [feat] GA Script 추가

--- apps/web/src/app/(pages)/layout.tsx
+++ apps/web/src/app/(pages)/layout.tsx
@@ -1,14 +1,23 @@
-'use client'
+"use client";
 
+import Script from "next/script";
 import Header from "../(components)/common/Header";
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', "${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}");
+        `}
+      </Script>
       <Header />
       <main>{children}</main>
     </>
