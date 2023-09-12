94f2acd45c523d1c69f9854805a1e650fd642d4f
ynawhocodes <ynawhocodes@gmail.com>
2023-09-12 11:46:01 +0900

    [mod] shop coming soon으로 변경

--- apps/web/src/app/(pages)/shop/page.tsx
+++ apps/web/src/app/(pages)/shop/page.tsx
@@ -13,10 +13,10 @@ const ShopPage = () => {
         <img src="/images/shop_title.png" alt="compcard" />
       </div>
       <div className="flex justify-center">
-        <div className="px-4 py-2.5 grid grid-cols-2 gap-2.5 lg:w-1/2 w-full">
-          {/* <div className="aspect-[2/3] flex items-center justify-center">
-            <img src="/images/comingsoon.png" alt="comingsoon" />
-          </div> */}
+        <div className="aspect-[2/3] flex items-center justify-center">
+          <img src="/images/comingsoon.png" alt="comingsoon" />
+        </div>
+        {/* <div className="px-4 py-2.5 grid grid-cols-2 gap-2.5 lg:w-1/2 w-full">
           {shops?.map((shop) => (
             <div
               className="relative group"
@@ -34,7 +34,7 @@ const ShopPage = () => {
               )}
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </>
   );
