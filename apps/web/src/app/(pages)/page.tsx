14df9e65c0fa426377d2aecab7ada7ec4cba5c38
yanwhocodes <ynawhocodes@gmail.com>
2023-09-23 00:50:10 +0900

    [fix] home 중앙정렬 이슈 해결

--- apps/web/src/app/(pages)/page.tsx
+++ apps/web/src/app/(pages)/page.tsx
@@ -6,29 +6,27 @@ import { useState } from "react";
 export default function Home() {
   const [onToggle, setOnToggle] = useState(false);
   return (
-    <>
+    <div className="h-full">
       {onToggle ? (
-        <>
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center h-full">
             <img
               className="animate-fade-in w-1/3 max-w-[200px]"
               src="/images/main.png"
               alt="main"
             />
           </div>
-        </>
       ) : (
-        <div onClick={() => setOnToggle(true)}>
+        <div className="h-full" onClick={() => setOnToggle(true)}>
           <div className="fixed top-0 z-50 h-10 p-4 bg-white">
             <img className="w-[20px]" src="/images/bar_3x.png" alt="bar" />
           </div>
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center h-full">
             <div>
               <img className="w-[90px]" src="/images/logo_3x.png" alt="arinniee logo" />
             </div>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
