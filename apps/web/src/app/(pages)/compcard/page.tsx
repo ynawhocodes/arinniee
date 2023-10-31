a1282863a26a0287c622204c59fa84ba8be2bffe
ynawhocodes <ynawhocodes@gmail.com>
2023-10-31 18:06:15 +0900

    [feat] compard page 이미지 삭제 기능 추가

--- apps/web/src/app/(pages)/compcard/page.tsx
+++ apps/web/src/app/(pages)/compcard/page.tsx
@@ -6,11 +6,13 @@ import ImageSlider from "../../(components)/ImageSlider";
 import useCompcard from "../../_hooks/use-compcard";
 import Image from "next/image";
 import TempUploader from "../../(components)/common/TempUploader";
+import { deleteCompcard } from "../../_api/compcard/deleteCompard";
 
 const CompcardPage = () => {
   const { compcards } = useCompcard();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [clickImageIndex, setClickImageIndex] = useState(0);
+  const [isCancleMode, setIsCancleMode] = useState(false);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -50,18 +52,35 @@ const CompcardPage = () => {
       {/* <div className="flex items-center justify-center ">
         <img src="/images/comingsoon.png" alt="comingsoon" />
       </div> */}
+
+      {/* 어드민 */}
+      {/* <TempUploader folder="compcard" /> */}
+      {/* <button onClick={() => setIsCancleMode(!isCancleMode)}>
+        게시글 삭제 모드 {isCancleMode ? 'OFF' : 'ON'}
+      </button> */}
       <div className="flex justify-center">
         <div className="px-4 py-2.5 grid grid-cols-3 gap-2.5 w-full max-w-[600px]">
           {compcards?.map((compcard, index) => (
-            <div
-              key={compcard.id}
-              onClick={() => {
-                openModal();
-                setClickImageIndex(index);
-              }}
-            >
+            <div key={compcard.id} className="relative">
+              {isCancleMode && (
+                <div
+                  className="absolute top-[-10px] left-[-10px] flex items-center justify-center w-6 bg-gray-400 rounded-full aspect-square z-10"
+                  onClick={async () => {
+                    console.log(compcard.id);
+                    // await deleteCompcard(compcard.id);
+                  }}
+                >
+                  <p className="text-white">X</p>
+                </div>
+              )}
               <Image
-                className="aspect-[1/1] bg-gray-300 object-cover"
+                className={`aspect-[1/1] bg-gray-300 object-cover ${
+                  isCancleMode ? `animate-wiggle` : ``
+                }`}
+                onClick={() => {
+                  openModal();
+                  setClickImageIndex(index);
+                }}
                 src={compcard.thumbnailImageUrl}
                 alt="film thumbnail"
                 width={300}
