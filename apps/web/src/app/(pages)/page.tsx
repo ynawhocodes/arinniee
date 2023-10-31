07309197b6015b095607c99ed2aec2d48af69a7c
ynawhocodes <ynawhocodes@gmail.com>
2023-10-31 17:29:38 +0900

    [mod] home에 logo만 있도록 변경

--- apps/web/src/app/(pages)/page.tsx
+++ apps/web/src/app/(pages)/page.tsx
@@ -4,31 +4,11 @@ import Header from "../(components)/common/Header";
 import { useState } from "react";
 
 export default function Home() {
-  const [onToggle, setOnToggle] = useState(false);
   return (
-    <>
-      {onToggle ? (
-        <>
-          <div className="flex items-center justify-center h-screen">
-            <img
-              className="animate-fade-in w-1/3 max-w-[200px]"
-              src="/images/main.png"
-              alt="main"
-            />
-          </div>
-        </>
-      ) : (
-        <div onClick={() => setOnToggle(true)}>
-          <div className="fixed top-0 z-50 h-10 p-4 bg-white">
-            <img src="/images/bar.png" alt="bar" />
-          </div>
-          <div className="flex items-center justify-center h-screen">
-            <div>
-              <img src="/images/logo.png" alt="arinniee logo" />
-            </div>
-          </div>
-        </div>
-      )}
-    </>
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <img src="/images/logo.png" alt="arinniee logo" />
+      </div>
+    </div>
   );
 }
