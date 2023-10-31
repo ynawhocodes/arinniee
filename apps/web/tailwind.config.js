b882ff19922e0995379a7d2feb88cb9643c1f5db
ynawhocodes <ynawhocodes@gmail.com>
2023-10-31 18:05:33 +0900

    [feat] wiggle animation 추가

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -4,4 +4,17 @@ const sharedConfig = require("tailwind-config/tailwind.config.js");
 
 module.exports = {
   presets: [sharedConfig],
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      }
+    }
+  }
 };
